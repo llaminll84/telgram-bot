135017f30c019fd072d45ede9353b0edf831aa9a
llaminll84 <159070064+llaminll84@users.noreply.github.com>
2025-09-14 04:01:40 +0330

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -8,8 +9,5 @@ def home():
     return "✅ ربات فعال است!"
 
 def run():
-    app.run(host='0.0.0.0', port=8000)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
