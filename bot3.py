427967f4751348f6bdae8bf54dbb7127c25b1c12
llaminll84 <159070064+llaminll84@users.noreply.github.com>
2025-09-18 15:09:06 +0330

    Update bot3.py

--- bot3.py
+++ bot3.py
@@ -19,7 +19,7 @@ bot.send_message(chat_id=CHAT_ID, text="✅ ربات با موفقیت راه‌
 
 # ─── صرافی کوکوین ───
 exchange = ccxt.kucoin()
-TOP_N = 60
+TOP_N = 85
 TIMEFRAMES = ['5m', '15m', '1h']
 
 # گرفتن نمادهای برتر
